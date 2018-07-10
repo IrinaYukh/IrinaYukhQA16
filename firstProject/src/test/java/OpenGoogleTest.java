88be06e30d848289e2043beec9826b21400ce59c
IrinaYukh <40866840+IrinaYukh@users.noreply.github.com>
2018-07-10 08:28:34 +0300

    created test open / close Google web page

---
+++ firstProject/src/test/java/OpenGoogleTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class OpenGoogleTest
+{
+    public  WebDriver driver = null;
+
+    @BeforeMethod
+    public void setUp()
+    {
+        driver = new ChromeDriver();
+    }
+@Test
+public void siteOpeningTest(){
+    //driver.get("https://www.google.com/");
+    driver.get("https://www.google.com");
+}
+
+@AfterMethod
+public void tearDown(){
+    driver.quit();
+}
+
+
+}
