3c9492ff7fed982b18762b9b3d4ddaa51dc7d9e6
IrinaYukh <40866840+IrinaYukh@users.noreply.github.com>
2018-07-12 15:04:25 +0300

    created simple own Test1

--- firstProject/src/test/java/SimpleTest.java
+++ firstProject/src/test/java/SimpleTest.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SimpleTest
@@ -8,8 +10,20 @@ public class SimpleTest
     public void navigateToQALightWebSite()
     {
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize(); // Web browser window will open on max.size
-        driver.get("https://qalight.com.ua/");
-        driver.quit();
+        //WebDriver driver = new FirefoxDriver();
+        if (driver != null)
+        {
+            for (int i = 0; i<50; i++)
+            {
+                //driver.manage().window().setSize(new Dimension(100,200));
+                driver.manage().window().maximize(); // Web browser window will open on max.size
+                driver.get("https://qalight.com.ua/");
+            }
+            driver.quit();
+        }
+        else
+        {
+            System.out.println("Empty");
+        }
     }
 }
