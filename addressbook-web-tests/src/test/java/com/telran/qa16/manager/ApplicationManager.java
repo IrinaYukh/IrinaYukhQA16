b3904fa4c288a6542fc818c1cb6448c29801d3f3
IrinaYukh <40866840+IrinaYukh@users.noreply.github.com>
2018-08-13 19:58:43 +0300

    Defining functions and variables to run tests from different browsers

--- addressbook-web-tests/src/test/java/com/telran/qa16/manager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/telran/qa16/manager/ApplicationManager.java
@@ -2,6 +2,8 @@ package com.telran.qa16.manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,11 +14,25 @@ public class ApplicationManager
     GroupHelper groupHelper;
 
     private WebDriver wd;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
 
     public void start()
     {
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME))
+        {
+            wd = new ChromeDriver();
+        }
+        else if(browser.equals(BrowserType.FIREFOX))
+        {
+            wd=new FirefoxDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         sessionHelper = new SessionHelper(wd);
         sessionHelper.openSite("http://localhost/addressbook/");
@@ -28,8 +44,6 @@ public class ApplicationManager
     public void stop() {
         wd.quit();
     }
-
-
 
 
 
--- addressbook-web-tests/src/test/java/com/telran/qa16/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/telran/qa16/tests/TestBase.java
@@ -1,13 +1,15 @@
 package com.telran.qa16.tests;
 
 import com.telran.qa16.manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase
 {
 
-    public static ApplicationManager app = new ApplicationManager();
+    public static ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp(){
